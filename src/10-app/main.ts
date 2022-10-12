3cb2f82bbe8601fe38d5465cb517ba68fa81c044
jl-molina <joseluisml1218@gmail.com>
2022-10-12 11:06:01 -0600

    extend interfaces class

--- src/10-app/main.ts
+++ src/10-app/main.ts
@@ -3,11 +3,14 @@ import { addProduct } from './products/products.service';
 addProduct({
     id: 1,
     title: 'p1',
-    createdAt: new Date,
+    createdAt: new Date(),
+    updatedAt: new Date(),
     stock: 10,
-    // size: 'M'
+    size: 'M',
     category: {
         id: 'producto 2',
+        createdAt: new Date(),
+        updatedAt:  new Date(),
         name: 'chair'
     }
 });
--- src/10-app/orders/order.model.ts
+++ src/10-app/orders/order.model.ts
@@ -1,9 +1,8 @@
 import { User } from "./../users/user.model";
 import { Product } from "./../products/products.model"
+import { BaseModel } from "../base.model";
 
-interface Order {
-    id: string | number,
-    createdAt: Date,
+interface Order extends BaseModel{
     products: Product[],
     user: User
 }
