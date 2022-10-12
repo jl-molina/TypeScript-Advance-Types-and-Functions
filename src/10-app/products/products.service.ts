c2bbdbe37657386ff1c64a570e64032dc9f612d8
jl-molina <joseluisml1218@gmail.com>
2022-10-12 11:14:46 -0600

    readonly class

--- src/10-app/products/products.service.ts
+++ src/10-app/products/products.service.ts
@@ -3,5 +3,6 @@ import { Product } from './products.model';
 const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
+    data.updatedAt = new Date();
     products.push(data);
 }
